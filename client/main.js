0f5e5a70c022bb950dce406b45529d415694bf42
Lucas Pirola <lucaspirola@gmail.com>
2018-08-15 01:00:50 -0300

    Update main client entrypoiny

---
+++ client/main.js
@@ -0,0 +1,21 @@
+// Client entry point, imports all client code
+
+import '/imports/startup/client';
+import '/imports/startup/both';
+
+Meteor.startup(function() {
+    $('html').attr("xml:lang", "pt-br");
+    $('html').attr("lang", "pt-br");
+    require(['chartist-plugin-legend']);
+
+    Uploader.finished = function(index, fileInfo, body) {
+      var f_type = body.data.formData.file_type;
+      Session.set(f_type, fileInfo.path);
+    }
+
+    FlashMessages.configure({
+      autoHide: true,
+      hideDelay: 4000,
+      autoScroll: true
+    });
+  })
