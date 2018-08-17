6a2e6c836d1e83602f1ac0529cc6b1a883c96680
Lucas Pirola <lucaspirola@gmail.com>
2018-08-17 03:34:22 -0300

    Improve code style with auto-fix

--- imports/api/films/films.js
+++ imports/api/films/films.js
@@ -6,56 +6,56 @@ Films.friendlySlugs({
   slugFrom: 'title',
   slugField: 'slug',
   distinct: true,
-  updateSlug: true
+  updateSlug: true,
 });
 
 Films.portfolio = function () {
   return Films.find({
-    status: "Portfolio"
+    status: 'Portfolio',
   });
-}
+};
 
 Films.disseminate = function () {
   return Films.find({
     $or: [{
-      status: 'Difusão'
+      status: 'Difusão',
     }, {
-      status: 'Difusão/Portfolio'
-    }]
+      status: 'Difusão/Portfolio',
+    }],
   }).fetch();
-}
-Films.all = function() {
+};
+Films.all = function () {
   return Films.find({}, {
     sort: {
-      sequence_number: 1
-    }
+      sequence_number: 1,
+    },
   });
-}
+};
 Films.active = function () {
-  return Films.find({status: {$not: /Oculto/}}, {
+  return Films.find({ status: { $not: /Oculto/ } }, {
     sort: {
-      sequence_number: 1
-    }
+      sequence_number: 1,
+    },
   });
-}
+};
 Films.count = function () {
   return Films.active().count();
-}
+};
 Films.by_user_id = function () {
-  var films = Films.find({
-    status: {$not: /Oculto/},
-    "screening.user_id": Meteor.userId()
+  const films = Films.find({
+    status: { $not: /Oculto/ },
+    'screening.user_id': Meteor.userId(),
   }).fetch();
   return films;
-}
+};
 
 Films.screenings_by_user_id = function () {
-  var user_id = Meteor.userId();
-  var films = Films.by_user_id(user_id);
-  var user_screenings = []
+  const user_id = Meteor.userId();
+  const films = Films.by_user_id(user_id);
+  const user_screenings = [];
 
   for (a = 0; a < films.length; a++) {
-    var f_scr = films[a].screening
+    const f_scr = films[a].screening;
     for (i = 0; i < f_scr.length; i++) {
       if (f_scr[i].user_id == user_id) {
         f_scr[i].title = films[a].title;
@@ -66,47 +66,47 @@ Films.screenings_by_user_id = function () {
     }
   }
   return user_screenings;
-}
+};
 
 Films.by_screening_id = function (screening_id) {
   return Films.findOne({
-    status: {$not: /Oculto/},
-    "screening._id": screening_id
+    status: { $not: /Oculto/ },
+    'screening._id': screening_id,
   });
-}
+};
 Films.return_film_and_screening = function (screening_id) {
-  var film = Films.by_screening_id(screening_id);
-  var screening = Films.return_screening(screening_id);
+  const film = Films.by_screening_id(screening_id);
+  const screening = Films.return_screening(screening_id);
   return {
     film,
-    screening
+    screening,
   };
-}
+};
 Films.return_screening = function (screening_id) {
-  var film = Films.by_screening_id(screening_id);
-  for (i = 0; i < film["screening"].length; i++) {
-    if (film["screening"][i]._id == screening_id) {
-      var screening = film["screening"][i]
+  const film = Films.by_screening_id(screening_id);
+  for (i = 0; i < film.screening.length; i++) {
+    if (film.screening[i]._id == screening_id) {
+      var screening = film.screening[i];
     }
   }
   return screening;
-}
+};
 Films.get_image_by_src = function (id, src) {
-  var film = Films.findOne(id),
+  let film = Films.findOne(id),
     image;
 
-  _.each(film.slideshow, function (img) {
+  _.each(film.slideshow, (img) => {
     if (img.src == src) {
       image = img;
     }
-  })
+  });
 
   return image;
-}
+};
 Films.inventory = function (film) {
   console.debug(film);
-  var legacyData = FilmScreeningInventory[film.title]
-  var screenings = film["screening"] || [],
+  const legacyData = FilmScreeningInventory[film.title];
+  let screenings = film.screening || [],
     initialInventory = {
       viewers: 0,
       viewers_from_reports: 0,
@@ -123,27 +123,26 @@ Films.inventory = function (film) {
       viewers_zones: {},
       past_sessions: 0,
       future_sessions: 0,
-      film: film
+      film,
     };
-  var inventory = $.extend({}, initialInventory, legacyData);
+  const inventory = $.extend({}, initialInventory, legacyData);
 
-  if(legacyData) {
-    inventory.legacy_viewers = legacyData.viewers
+  if (legacyData) {
+    inventory.legacy_viewers = legacyData.viewers;
   }
 
-  var now = new Date(),
+  let now = new Date(),
     states = [],
     cities = [],
     users = [];
   inventory.scheduled_sessions += screenings.length;
 
-  var scr_id_real = []
+  const scr_id_real = [];
   _.each(screenings, function (screening) {
-    //sessões - Sessões que não são rascunho
-    var real_quorum = ('real_quorum' in screening) ? screening['real_quorum'].replace(/[^0-9]/g, '') || 0 : 0;
+    // sessões - Sessões que não são rascunho
+    let real_quorum = ('real_quorum' in screening) ? screening.real_quorum.replace(/[^0-9]/g, '') || 0 : 0;
     real_quorum = parseInt(real_quorum);
     if (!('draft' in screening) || ('draft' in screening && screening.draft == false)) {
-
       // sessoes jah exibidas
       if (screening.date < now) {
         inventory.past_sessions += 1;
@@ -153,7 +152,7 @@ Films.inventory = function (film) {
           inventory.sessions_with_reports = parseInt(inventory.sessions_with_reports) + 1;
         }
       } else {
-        //sessoes a serem exibidas
+        // sessoes a serem exibidas
         inventory.future_sessions += 1;
       }
 
@@ -168,17 +167,17 @@ Films.inventory = function (film) {
 
       // Estados e viewers por area
       if ('uf' in screening) {
-        states.push(screening['uf']);
+        states.push(screening.uf);
         if (getZoneByState(screening.uf)) {
           incrementOrCreate(inventory.viewers_zones, getZoneByState(screening.uf));
         }
       }
 
-      //quais cidades
-      cities.push(screening['city']);
+      // quais cidades
+      cities.push(screening.city);
 
       // Usuários
-      if (screening['user_id']) {
+      if (screening.user_id) {
         users.push(Meteor.users.findOne(screening.user_id));
       }
     }
@@ -200,25 +199,25 @@ Films.inventory = function (film) {
   if (inventory.sessions > 0) {
     return inventory;
   }
-}
+};
 
-var getFileBlob = function (url, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.responseType = "blob";
-  xhr.addEventListener('load', function () {
+const getFileBlob = function (url, cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.addEventListener('load', () => {
     cb(xhr.response);
   });
   xhr.send();
 };
 
-var blobToFile = function (blob, name) {
+const blobToFile = function (blob, name) {
   blob.lastModifiedDate = new Date();
   blob.name = name;
   return blob;
 };
 
-var getFileObject = function (filePathOrUrl, cb) {
+const getFileObject = function (filePathOrUrl, cb) {
   getFileBlob(filePathOrUrl, function (blob) {
     cb(blobToFile(blob, 'test.jpg'));
   });
@@ -232,28 +231,28 @@ function incrementOrCreate(obj, key, increment) {
 }
 
 var getMonthName = function (month) {
-  var monthNames = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+  const monthNames = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro',
   ];
 
   return monthNames[month];
-}
+};
 
 var getZoneByState = function (state) {
-  var zones = {
-    'Sudeste': ['SP', 'ES', 'MG', 'RG'],
-    'Sul': ['PR', 'SC', 'RS'],
+  const zones = {
+    Sudeste: ['SP', 'ES', 'MG', 'RG'],
+    Sul: ['PR', 'SC', 'RS'],
     'Centro-Oeste': ['DF', 'GO', 'MS', 'MT'],
-    'Nordeste': ['BA', 'AL', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE'],
-    'Norte': ['AM', 'PA', 'RO', 'RR', 'AC', 'AP', 'TO']
+    Nordeste: ['BA', 'AL', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE'],
+    Norte: ['AM', 'PA', 'RO', 'RR', 'AC', 'AP', 'TO'],
   };
 
   _.each(zones, function (states, zone) {
     if (states.indexOf(state) > 0) {
       return zone;
     }
-  })
-}
+  });
+};
 
 // End of inventory functions
 
@@ -262,14 +261,14 @@ Films.allow({
   insert: function (userId, doc) {
     // only allow posting if you are logged in
     return !!userId;
-  }
+  },
 });
 
 if (Meteor.isClient) {
-  Meteor.subscribe("films");
+  Meteor.subscribe('films');
   Template.films.helpers({
     films: function () {
       return Films.active();
-    }
+    },
   });
 }
