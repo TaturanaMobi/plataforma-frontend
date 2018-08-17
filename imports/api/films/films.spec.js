86e91c8303f1c5b97ab134411215d6882b3bbb10
Lucas Pirola <lucaspirola@gmail.com>
2018-08-17 18:40:17 -0300

    Add test helper and improve ci config

--- imports/api/films/films.spec.js
+++ imports/api/films/films.spec.js
@@ -6,24 +6,22 @@ import { Factory } from 'meteor/dburles:factory';
 import { assert } from 'chai';
 // import { Random } from 'meteor/random';
 // import { _ } from 'meteor/underscore';
-// import { Todos } from '../todos.js';
+import { Films } from './films.js';
 // import './publications.js';
 
-describe('todos', function () {
-  describe('mutators', function () {
-    it('builds correctly from factory', function () {
-      const todo = Factory.create('todo');
-      assert.typeOf(todo, 'object');
-      assert.typeOf(todo.createdAt, 'date');
-    });
+describe('films', function () {
+  it('load correct from db', function () {
+    // console.log(Films);
+    assert.typeOf(Films, 'object');
+    // assert.typeOf(Films[0].get(), 'date');
   });
-  it('leaves createdAt on update', function () {
-    const createdAt = new Date(new Date() - 1000);
-    let todo = Factory.create('todo', { createdAt });
-    const text = 'some new text';
-    Todos.update(todo, { $set: { text } });
-    todo = Todos.findOne(todo._id);
-    assert.equal(todo.text, text);
-    assert.equal(todo.createdAt.getTime(), createdAt.getTime());
-  });
+  // it('leaves createdAt on update', function () {
+  //   const createdAt = new Date(new Date() - 1000);
+  //   let films = Factory.create('films', { createdAt });
+  //   const text = 'some new text';
+  //   Films.update(films, { $set: { text } });
+  //   films = Films.findOne(films._id);
+  //   assert.equal(films.text, text);
+  //   assert.equal(films.createdAt.getTime(), createdAt.getTime());
+  // });
 });
