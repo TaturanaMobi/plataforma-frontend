ba97e3b6e7c379ebb3abd1c5785a2bd873ecc705
Lucas Pirola <lucaspirola@gmail.com>
2018-09-08 07:01:58 -0300

    chore(grid): Improve how we use bootstrap3 grid

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,34 +1,12 @@
-import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { $ } from 'meteor/jquery';
 import { Router } from 'meteor/iron:router';
 
-import { Films } from '../../api/films/films.js';
-
-import './../components/carousel.html';
+import './../components/carousel.js';
 import './../components/carousel-item.html';
 import './home.html';
-
-Template.homeCarousel.onRendered(() => {
-  $('#carousel').slick({
-    arrows: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 6000,
-  });
-});
 
 Template.home.events({
   'click #home-films button': () => {
     Router.go('/screenings');
-  },
-});
-
-Template.homeCarousel.helpers({
-  disseminate() {
-    return Films.find({ status: 'Difusão' });
-  },
-  inventory() {
-    return Films.inventory(Template.currentData());
   },
 });
