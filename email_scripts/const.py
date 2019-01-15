0e401be16bbeb60616c54427d40eca85534c83ab
Lucas Pirola <lucaspirola@gmail.com>
2019-01-15 20:07:23 -0200

    chore(clean): Remove unused python email queue

--- email_scripts/const.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-from __future__ import (
-    absolute_import, division, print_function, unicode_literals
-)
-
-ABSOLUTE_URL = 'http://taturanamobi.com.br/'
-FROM = "Plataforma Taturana <taturanamobi@gmail.com>"
