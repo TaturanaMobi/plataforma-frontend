963fe8ad33c5b5b48c1b9a2f17b00f81cd6972ca
Lucas Pirola <lucaspirola@gmail.com>
2019-05-07 16:52:56 -0300

    chore(portfolio): Update en version

--- imports/models/images.js
+++ imports/models/images.js
@@ -3,7 +3,7 @@ import { FilesCollection } from 'meteor/ostrio:files';
 const Images = new FilesCollection({
   collectionName: 'Images',
   allowClientCode: false, // Disallow remove files from Client
-  storagePath: () => `${process.env.PWD}/uploads`,
+  storagePath: () => `${process.env.PWD}/uploads/images`,
   onBeforeUpload(file) {
     // Allow upload files under 10MB, and only in png/jpg/jpeg formats
     if (file.size <= 10485760 && /png|jpg|jpeg|zip|rar|doc|pdf|gif|ppt/i.test(file.extension)) {
