02ec8ea84167f9c664dab1d358a5ed554e402c93
Viviane <viviane.dias@protonmail.com>
2019-06-06 21:57:33 -0300

    [ADD] Added the flag to navbar

--- imports/ui/components/nav.js
+++ imports/ui/components/nav.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Router } from 'meteor/iron:router';
+import i18n from 'meteor/universe:i18n';
 
 import './nav.html';
 
@@ -11,4 +12,9 @@ Template.nav.events({
       Router.go('login');
     });
   },
+  'click .nav__translation-flag--img'(event) {
+    event.preventDefault();
+    Router.go('en');
+  },
 });
+Template.nav.calendar = 'CALENDÁRIO';
