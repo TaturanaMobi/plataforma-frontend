d90081a137603876e0d3c97bc2418c27c861e701
Lucas Pirola <lucaspirola@gmail.com>
2019-11-28 11:31:17 -0300

    chore(films): Add helper to trailer player markup
    vimeo and youtube supported

--- imports/startup/client/helpers.js
+++ imports/startup/client/helpers.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { moment } from 'meteor/momentjs:moment';
 import { Router } from 'meteor/iron:router';
+import { Spacebars } from 'meteor/spacebars';
 
 import Schemas from '../../models/schemas';
 import Films from '../../models/films';
@@ -10,6 +11,35 @@ import NotificationTemplates from '../../models/notification_templates';
 import Notifications from '../../models/notifications';
 import Screenings from '../../models/screenings';
 import Images from '../../models/images';
+
+function youtubeParser(url) {
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  const match = url.match(regExp);
+  return (match && match[7].length === 11) ? match[7] : false;
+}
+
+function vimeoParser(url) {
+  // Look for a string with 'vimeo', then whatever, then a
+  // forward slash and a group of digits.
+  const match = /vimeo.*\/(\d+)/i.exec(url);
+
+  // If the match isn't null (i.e. it matched)
+  if (match) {
+    // The grouped/matched digits from the regex
+    return match[1];
+  }
+  return false;
+}
+
+Template.registerHelper('trailerParser', function (url) {
+  const isYoutube = url.indexOf('youtu') > 0;
+  const videoId = isYoutube ? youtubeParser(url) : vimeoParser(url);
+
+  if (isYoutube) {
+    return new Spacebars.SafeString(`<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="${videoId}"></div>`);
+  }
+  return new Spacebars.SafeString(`<div id="player" data-plyr-provider="vimeo" data-plyr-embed-id="${videoId}"></div>`);
+});
 
 Template.registerHelper('currentRouteIs', function (route) {
   return Router.current().route.getName() === route;
