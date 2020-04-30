9fb3ccfd9edc503e5deda8e0da7dd9375c58a3a8
Lucas Pirola <lucaspirola@gmail.com>
2020-04-30 00:38:14 -0300

    Update sentry dsn

--- server/sentry.js
+++ server/sentry.js
@@ -1,3 +1,3 @@
 const Sentry = require('@sentry/node');
 
-Sentry.init({ dsn: 'https://7702d65e87ae44f19c52388506a15662@sentry.io/1329612' });
+Sentry.init({ dsn: 'https://7702d65e87ae44f19c52388506a15662@o208280.ingest.sentry.io/1329612' });
