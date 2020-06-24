def832737c049a1937c76439ef8d40cc02b60f84
Lucas Pirola <lucaspirola@gmail.com>
2020-06-23 21:58:00 -0300

    Add queue to async operations

--- imports/ui/pages/admin/adm-films-report.js
+++ imports/ui/pages/admin/adm-films-report.js
@@ -4,7 +4,7 @@ import { Template } from 'meteor/templating';
 // import * as docx from "docx";
 import { Document, Packer, Paragraph, TextRun, HeadingLevel, Media } from "docx";
 import { saveAs } from 'file-saver';
-
+const { default: PQueue } = require('p-queue');
 import './adm-films-report.html';
 import Screenings from '../../../models/screenings.js';
 import Images from '../../../models/images';
@@ -116,6 +116,8 @@ const toDataURLPromise = promisify(toDataURL);
 // let loadScriptPromise = promisify(loadScript);
 // loadScriptPromise(...).then(...);
 
+const queue = new PQueue({ concurrency: 1 });
+
 Template.admFilmsReport.helpers({
   screenings() {
     const result = Screenings.find();
@@ -131,158 +133,160 @@ const fixImagePath = (imagePath) => {
 
 Template.admFilmsReport.events({
   'click .export-docx'() {
-    const doc = new Document();
-    const film = Template.instance().data;
-    const screenings = Screenings.find();
+    (async () => {
 
-    doc.addSection({
-      properties: {},
-      children: [
-        new Paragraph({
-          text: film.title,
-          heading: HeadingLevel.HEADING_1,
-        })
-      ]
-    });
+      const doc = new Document();
+      const film = Template.instance().data;
+      const screenings = Screenings.find();
 
-    // const asyncForEach = async (array, callback) => {
-    //   for (let index = 0; index < array.length; index++) {
-    //     await callback(array[index], index, array);
-    //   }
-    // }
-
-    // for (const element in screenings) {
-    screenings.forEach(async (element) => {
-      // for (var i = 0, len = screenings.length; i < len; i++) {
-      // const start = async () => {
-      // asyncForEach(screenings, async (element) => {
-      //   await waitFor(50);
-      //   console.log(num);
-      // })
-
-      // const element = screenings[i];
-      // let image1 = '';
-      // if (typeof element.report_image_1 !== 'undefined') {
-      //   image1 = await toDataURLPromise(`${Meteor.settings.public.imageServerUrl}/fit?width=300&height=300&type=jpeg&file=${fixImagePath(element.report_image_1)}`);
-      // } else {
-      //   image1 = dotImage;
-      // }
-      // let image2 = '';
-      // if (element.report_image_2) {
-      //   image2 = await toDataURLPromise(`${Meteor.settings.public.imageServerUrl}/fit?width=300&height=300&type=jpeg&file=${fixImagePath(element.report_image_2)}`);
-      // } else {
-      //   image2 = dotImage;
-      // }
-      // let image3 = '';
-      // if (element.report_image_3) {
-      //   image3 = await toDataURLPromise(`${Meteor.settings.public.imageServerUrl}/fit?width=300&height=300&type=jpeg&file=${fixImagePath(element.report_image_3)}`);
-      // } else {
-      //   image3 = dotImage;
-      // }
-      const ambassador = element.ambassador();
-
-      const docImage1 = Media.addImage(doc, element.report_image_1 ? await makeRequest('GET', `${Meteor.settings.public.imageServerUrl}/smartcrop?width=600&height=338&type=jpeg&file=${fixImagePath(element.report_image_1)}`) : dotImage, 600, 338);
-      const docImage2 = Media.addImage(doc, element.report_image_2 ? await makeRequest('GET', `${Meteor.settings.public.imageServerUrl}/smartcrop?width=600&height=338&type=jpeg&file=${fixImagePath(element.report_image_2)}`) : dotImage, 600, 338);
-      const docImage3 = Media.addImage(doc, element.report_image_3 ? await makeRequest('GET', `${Meteor.settings.public.imageServerUrl}/smartcrop?width=600&height=338&type=jpeg&file=${fixImagePath(element.report_image_3)}`) : dotImage, 600, 338);
-      // console.log(element);
       doc.addSection({
         properties: {},
         children: [
           new Paragraph({
+            text: film.title,
+            heading: HeadingLevel.HEADING_1,
+          })
+        ]
+      });
+
+      // const asyncForEach = async (array, callback) => {
+      //   for (let index = 0; index < array.length; index++) {
+      //     await callback(array[index], index, array);
+      //   }
+      // }
+
+      // for (const element in screenings) {
+      screenings.forEach(async (element) => {
+        // for (var i = 0, len = screenings.length; i < len; i++) {
+        // const start = async () => {
+        // asyncForEach(screenings, async (element) => {
+        //   await waitFor(50);
+        //   console.log(num);
+        // })
+
+        // const element = screenings[i];
+        // let image1 = '';
+        // if (typeof element.report_image_1 !== 'undefined') {
+        //   image1 = await toDataURLPromise(`${Meteor.settings.public.imageServerUrl}/fit?width=300&height=300&type=jpeg&file=${fixImagePath(element.report_image_1)}`);
+        // } else {
+        //   image1 = dotImage;
+        // }
+        // let image2 = '';
+        // if (element.report_image_2) {
+        //   image2 = await toDataURLPromise(`${Meteor.settings.public.imageServerUrl}/fit?width=300&height=300&type=jpeg&file=${fixImagePath(element.report_image_2)}`);
+        // } else {
+        //   image2 = dotImage;
+        // }
+        // let image3 = '';
+        // if (element.report_image_3) {
+        //   image3 = await toDataURLPromise(`${Meteor.settings.public.imageServerUrl}/fit?width=300&height=300&type=jpeg&file=${fixImagePath(element.report_image_3)}`);
+        // } else {
+        //   image3 = dotImage;
+        // }
+        const ambassador = element.ambassador();
+
+        // await queue.add(() => {got('https://sindresorhus.com')});
+
+        await queue.add(async () => {
+
+          const docImage1 = Media.addImage(doc, element.report_image_1 ? await makeRequest('GET', `${Meteor.settings.public.imageServerUrl}/smartcrop?width=600&height=338&type=jpeg&file=${fixImagePath(element.report_image_1)}`) : dotImage, 600, 338);
+          const docImage2 = Media.addImage(doc, element.report_image_2 ? await makeRequest('GET', `${Meteor.settings.public.imageServerUrl}/smartcrop?width=600&height=338&type=jpeg&file=${fixImagePath(element.report_image_2)}`) : dotImage, 600, 338);
+          const docImage3 = Media.addImage(doc, element.report_image_3 ? await makeRequest('GET', `${Meteor.settings.public.imageServerUrl}/smartcrop?width=600&height=338&type=jpeg&file=${fixImagePath(element.report_image_3)}`) : dotImage, 600, 338);
+          // console.log(element);
+          doc.addSection({
+            properties: {},
             children: [
-              new TextRun({
-                text: "Data:\n",
-                bold: true,
+              new Paragraph({
+                children: [
+                  new TextRun({
+                    text: "Data:\n",
+                    bold: true,
+                  }),
+                  new TextRun(`${element.date}`),
+                ],
               }),
-              new TextRun(`${element.date}`),
-            ],
-          }),
-          new Paragraph({
-            spacing: {
-              before: 200,
-            },
-            children: [
-              new TextRun({
-                text: "Local:\n",
-                bold: true,
-              }),
-              new TextRun(`${element.place_name}
+              new Paragraph({
+                spacing: {
+                  before: 200,
+                },
+                children: [
+                  new TextRun({
+                    text: "Local:\n",
+                    bold: true,
+                  }),
+                  new TextRun(`${element.place_name}
                ${element.street} ${element.number} ${element.complement} ${element.zone}
                ${element.city} - ${element.uf}, ${element.s_country}`),
-            ],
-          }),
-          new Paragraph({
-            spacing: {
-              before: 200,
-            },
-            children: [
-              new TextRun({
-                text: "Embaixador:\n",
-                bold: true,
+                ],
               }),
-              new TextRun(`${ambassador.profile.name}
+              new Paragraph({
+                spacing: {
+                  before: 200,
+                },
+                children: [
+                  new TextRun({
+                    text: "Embaixador:\n",
+                    bold: true,
+                  }),
+                  new TextRun(`${ambassador.profile.name}
                ${ambassador.profile.cell_phone} / ${ambassador.profile.phone}
                ${ambassador.emails[0].address}`),
-            ],
-          }),
-          new Paragraph({
-            spacing: {
-              before: 200,
-            },
-            children: [
-              new TextRun({
-                text: "Público:\n",
-                bold: true,
+                ],
               }),
-              new TextRun(`esperado: ${element.quorum_expectation}
+              new Paragraph({
+                spacing: {
+                  before: 200,
+                },
+                children: [
+                  new TextRun({
+                    text: "Público:\n",
+                    bold: true,
+                  }),
+                  new TextRun(`esperado: ${element.quorum_expectation}
                presente:${element.real_quorum}`),
+                ],
+              }),
+              new Paragraph({
+                spacing: {
+                  before: 200,
+                },
+                children: [
+                  new TextRun({
+                    text: "Comentário:\n",
+                    bold: true,
+                  }),
+                  new TextRun(`${element.report_description}`),
+                ]
+              }),
+              new Paragraph({
+                spacing: {
+                  before: 200,
+                },
+                children: [
+                  new TextRun({
+                    text: "Fotos:\n",
+                    bold: true,
+                  }),
+                ]
+              }),
+              new Paragraph({
+                children: [
+                  (element.report_image_1 ? docImage1 : ''),
+                  (element.report_image_2 ? docImage2 : ''),
+                  (element.report_image_3 ? docImage3 : ''),
+                ]
+              })
             ],
-          }),
-          new Paragraph({
-            spacing: {
-              before: 200,
-            },
-            children: [
-              new TextRun({
-                text: "Comentário:\n",
-                bold: true,
-              }),
-              new TextRun(`${element.report_description}`),
-            ]
-          }),
-          new Paragraph({
-            spacing: {
-              before: 200,
-            },
-            children: [
-              new TextRun({
-                text: "Fotos:\n",
-                bold: true,
-              }),
-            ]
-          }),
-          new Paragraph({
-            children: [
-              (element.report_image_1 ? docImage1 : ''),
-              (element.report_image_2 ? docImage2 : ''),
-              (element.report_image_3 ? docImage3 : ''),
-            ]
-          })
-        ],
+          });
+        });
       });
-    });
 
-
-    console.log('feito')
-    setTimeout(() => {
-
-      Packer.toBlob(doc).then((blob) => {
-        // saveAs from FileSaver will download the file
-        saveAs(blob, `${film.title}-${new Date()}.docx`);
+      await queue.add(() => {
+        Packer.toBlob(doc).then((blob) => {
+          // saveAs from FileSaver will download the file
+          saveAs(blob, `${film.title}-${new Date()}.docx`);
+        });
       });
-    }, 10000)
-
-    // }
-    // start();
+    })();
   }
 })
